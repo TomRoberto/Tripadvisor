a9e1a98e313a6da0c961613269f5d4ea3c96ec42
Tom ROBERTO <tomroberto@Air-de-Tom.home>
2021-04-24 23:56:29 +0200

    js and send formulary

---
+++ assets/JS/main.js
@@ -0,0 +1,32 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+  console.log("Page chargée");
+
+  $.querySelector("#btn-display-form").addEventListener("click", () => {
+    $.querySelector("#modal").classList.add("display");
+  });
+
+  $.querySelector("#btn-close-form").addEventListener("click", () => {
+    $.querySelector("#modal").classList.add("hidden");
+    console.log("fermer");
+  });
+
+  $.querySelector("#submit").addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    const data = {
+      name: $.querySelector("#name").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      subject: $.querySelector("#subject").value,
+      message: $.querySelector("#message").value,
+    };
+    console.log(data);
+    try {
+      const response = await axios.post("http://localhost:3000/form", data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+});
